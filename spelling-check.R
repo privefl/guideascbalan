96c62cb063953d84319b60b1f50112f8d7c9a186
Florian Privé <florian.prive.21@gmail.com>
2026-02-02 14:00:23 +0100

    switch to quarto

--- spelling-check.R
+++ spelling-check.R
@@ -3,7 +3,7 @@ if (!exists("WORDS_TO_IGNORE")) WORDS_TO_IGNORE <- character(0)
 # Spelling check
 spelling_errors <- spelling::spell_check_files(
   path = `if`(interactive(), rstudioapi::getSourceEditorContext()$path,
-              knitr::current_input()),
+              gsub("\\.rmarkdown$", ".Rmd", knitr::current_input())),
   ignore = c(scan("WORDLIST", ""), WORDS_TO_IGNORE),
   lang = "fr_FR"
 )
